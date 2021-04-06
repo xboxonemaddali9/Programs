a3e70e7a979952127c58408da7673344aa6edb59
xboxonemaddali9 <72016797+xboxonemaddali9@users.noreply.github.com>
2021-04-06 16:39:28 +1000

    Youtube to MP3 Program Update
    Completed program upload

---
+++ Youtube to MP3 Converter/converter.py
@@ -0,0 +1,25 @@
+import youtube_dl
+import ffmpeg
+import os
+
+SAVE_PATH = '/'.join(os.getcwd().split('/')[:3]) + '/Downloads'
+
+def converter(_url):
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+        'outtmpl':SAVE_PATH + f'/%(playlist_title)s - Downloaded Songs/%(title)s.%(ext)s',
+        'quiet': True,
+        'ignoreerrors': True,
+    }
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([_url])
+if __name__ == "__main__":
+    vidURL = input("What is the link of the video/playlist? Make sure if it is playlist to make it public!\n>")
+    print("\nAll files will be saved in Downloads.\nDownloading files now... Please wait. It will take approximately 10 seconds per conversion.\nIf you close this program, simply use the same link again to continue download sequence.\n")
+    converter(vidURL)
